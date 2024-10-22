c91d523011b6bca4945eb967043891887a6438e2
usuario <usuario@BRI-LAB-NOTE-01>
2024-10-22 10:40:44 -0300

    Adicionar arquivos de projeto.

---
+++ IFSPStore.Domain/Base/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFSPStore.Domain.Base
+{
+    //uma classe abstrata não pode ser instanciada (criar outro objeto a partir dela [new "className])
+    public abstract class BaseEntity<TID>
+    {
+        protected BaseEntity() { }
+
+        protected BaseEntity(TID id) { //não obrigatório
+            Id = id;
+        }
+        public TID? Id { get; set; }
+    }
+}

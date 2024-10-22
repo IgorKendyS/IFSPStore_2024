abf7f3fac8ee6370f5a1cb5733f5e6b3d74a559e
IgorKendyS <igor-kendy@hotmail.com>
2024-10-22 11:49:52 -0300

    Testando os metodos..

---
+++ IFSPStore.Domain/Entities/Produto.cs
@@ -0,0 +1,30 @@
+﻿using IFSPStore.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFSPStore.Domain.Entities
+{
+    public class Produto : BaseEntity<int>
+    {
+        public Produto(int id, string nome, float preco, int quantidade, DateOnly dataCompra, string unidadeVenda, Grupo grupo) {
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+            DataCompra = dataCompra;
+            Preco = preco;
+            UnidadeVenda = unidadeVenda;
+            Grupo = grupo;
+        }
+
+        public string? Nome { get; set; }
+        public float? Preco { get; set; }
+        public int? Quantidade { get; set; }
+        public DateOnly? DataCompra { get; set; }
+        public string UnidadeVenda { get; set; }
+        public virtual Grupo? Grupo { get; set; }
+    }
+}
---
+++ IFSPStore.Domain/Entities/Venda.cs
@@ -0,0 +1,58 @@
+﻿using IFSPStore.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace IFSPStore.Domain.Entities
+{
+    public class Venda : BaseEntity<int>
+    {
+        public Venda() {
+            Items = new List<VendaItem>();
+        }
+        public Venda(int id, DateTime data, float valorTotal,
+            Usuario? usuario, Cliente? cliente, List<VendaItem> items) : base(id)
+        {
+            Id = id;
+            Data = data;
+            ValorTotal = valorTotal;
+            Usuario = usuario;
+            Cliente = cliente;
+            Items = items;
+        }
+
+        public DateTime? Data { get; set; }
+        public float? ValorTotal { get; set; }
+        public virtual Usuario? Usuario { get; set; }
+        public virtual Cliente cliente { get; set; }
+        public virtual List<VendasItem> Items { get; set; }
+
+    }
+
+    public class VendaItem : BaseEntity<int>
+    {
+
+        public VendaItem(int id, int quantidade, float valorUnitario, float valorTotal,
+            Produto produto, Venda venda)
+        {
+            Id = id;
+            Produto = produto;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+            ValorTotal = valorTotal;
+            Venda = venda;
+        }
+
+        public virtual Produto? Produto { get; set; }
+
+        public int Quantidade { get; set; }
+        public float ValorUnitario {  get; set; }
+        public float ValorTotal { get; set; }
+        [JsonIgnore]
+        public virtual Venda Venda { get; set; }
+    }
+}
